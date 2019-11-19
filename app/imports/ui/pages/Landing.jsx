7b68697f1bfcfbaaa146bda0d8ad0ab053d1ed70
John Bigornia <32423793+johnbigornia@users.noreply.github.com>
2019-11-19 13:37:11 -1000

    Landing page

--- app/imports/ui/pages/Landing.jsx
+++ app/imports/ui/pages/Landing.jsx
@@ -1,22 +1,28 @@
 import React from 'react';
-import { Grid, Image } from 'semantic-ui-react';
+import { Grid, Search, Button, Icon } from 'semantic-ui-react';
 
 /** A simple static component to render some text for the landing page. */
 class Landing extends React.Component {
   render() {
     return (
-        <Grid verticalAlign='middle' textAlign='center' container>
-
-          <Grid.Column width={4}>
-            <Image size='small' circular src="/images/meteor-logo.png"/>
-          </Grid.Column>
-
-          <Grid.Column width={8}>
-            <h1>Welcome to this template</h1>
-            <p>Now get to work and modify this app!</p>
-          </Grid.Column>
-
-        </Grid>
+        <div className='landing-background'>
+          <Grid container className='search-bar'>
+            <Grid.Row centered>
+              <Search
+                  placeholder='Start your club search...'
+                  icon='search icon'
+              />
+            </Grid.Row>
+            <Grid.Row centered>
+              <Button animated='vertical'>
+                <Button.Content hidden>
+                <Icon name='user' />
+                </Button.Content>
+                <Button.Content visible>Sign In</Button.Content>
+              </Button>
+            </Grid.Row>
+          </Grid>
+        </div>
     );
   }
 }
