fb8ad79e473664ad683b63a171a85628856bf62f
willjsimmons <willjsimmons@gmail.com>
2019-11-20 15:23:49 -1000

    working on search page

--- app/imports/ui/pages/SearchPage.jsx
+++ app/imports/ui/pages/SearchPage.jsx
@@ -1,21 +1,43 @@
 import React from 'react';
 import { Grid, Image, Icon, Card, Search } from 'semantic-ui-react';
 
+ let clubs =  {
+    "ClubName": "Accounting Club at UH Manoa",
+    "Type": "Academic/Professional",
+    "ContactName": "Chelsi Morishige",
+    "ContactEmail": "chelsicm@hawaii.edu",
+    "Website": "acuhmanoa.com",
+    "Email": "chelsicm@hawaii.edu",
+    "Description": "chelsicm@hawaii.edu"
+  }
 /** A simple static component to render some text for the landing page. */
 class SearchPage extends React.Component {
   render() {
     return (
-        <Grid verticalAlign='middle' textAlign='center' container>
+        <Grid verticalAlign='middle'container>
+	<Grid.Row>
 	<Search/>
-		<Card>
-			hello
-		</Card>
-	<Card>
-			hello
-		</Card>
-	<Card>
-			hello
-		</Card>
+	</Grid.Row>
+	<Grid.Row>
+<Card>
+    <Image fluid src="/image/meteor-logo.png"/>
+    <Card.Content>
+      <Card.Header>{clubs.ClubName}</Card.Header>
+      <Card.Meta>
+        <span className='date'>Joined in 2015</span>
+      </Card.Meta>
+      <Card.Description>
+        {clubs.Description}
+      </Card.Description>
+    </Card.Content>
+    <Card.Content extra>
+      <a>
+        <Icon name='user' />
+        22 Friends
+      </a>
+    </Card.Content>
+  </Card>
+	</Grid.Row>
         </Grid>
     );
   }
