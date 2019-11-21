2fa0f470e6ec1bdf7fdd908207cf26c6061c94b6
willjsimmons <willjsimmons@gmail.com>
2019-11-20 19:31:14 -1000

    added multiple cards

--- app/imports/ui/pages/SearchPage.jsx
+++ app/imports/ui/pages/SearchPage.jsx
@@ -8,7 +8,8 @@ import { Grid, Image, Icon, Card, Search } from 'semantic-ui-react';
     "ContactEmail": "chelsicm@hawaii.edu",
     "Website": "acuhmanoa.com",
     "Email": "chelsicm@hawaii.edu",
-    "Description": "chelsicm@hawaii.edu"
+    "Description": "chelsicm@hawaii.edu",
+    "Image" : "/images/meteor-logo.png"
   }
 /** A simple static component to render some text for the landing page. */
 class SearchPage extends React.Component {
@@ -19,8 +20,9 @@ class SearchPage extends React.Component {
 	<Search/>
 	</Grid.Row>
 	<Grid.Row>
+	<Card.Group>
 <Card>
-    <Image fluid src="/image/meteor-logo.png"/>
+    <Image fluid src={clubs.Image}/>
     <Card.Content>
       <Card.Header>{clubs.ClubName}</Card.Header>
       <Card.Meta>
@@ -37,6 +39,137 @@ class SearchPage extends React.Component {
       </a>
     </Card.Content>
   </Card>
+<Card>
+    <Image fluid src={clubs.Image}/>
+    <Card.Content>
+      <Card.Header>{clubs.ClubName}</Card.Header>
+      <Card.Meta>
+        <span className='date'>Joined in 2015</span>
+      </Card.Meta>
+      <Card.Description>
+        {clubs.Description}
+      </Card.Description>
+    </Card.Content>
+    <Card.Content extra>
+      <a>
+        <Icon name='user' />
+        22 Friends
+      </a>
+    </Card.Content>
+  </Card>
+<Card>
+    <Image fluid src={clubs.Image}/>
+    <Card.Content>
+      <Card.Header>{clubs.ClubName}</Card.Header>
+      <Card.Meta>
+        <span className='date'>Joined in 2015</span>
+      </Card.Meta>
+      <Card.Description>
+        {clubs.Description}
+      </Card.Description>
+    </Card.Content>
+    <Card.Content extra>
+      <a>
+        <Icon name='user' />
+        22 Friends
+      </a>
+    </Card.Content>
+</Card>
+<Card>
+    <Image fluid src={clubs.Image}/>
+    <Card.Content>
+      <Card.Header>{clubs.ClubName}</Card.Header>
+      <Card.Meta>
+        <span className='date'>Joined in 2015</span>
+      </Card.Meta>
+      <Card.Description>
+        {clubs.Description}
+      </Card.Description>
+    </Card.Content>
+    <Card.Content extra>
+      <a>
+        <Icon name='user' />
+        22 Friends
+      </a>
+    </Card.Content>
+</Card>
+    </Card.Group>
+	</Grid.Row>
+    <Grid.Row>
+	<Card.Group>
+<Card>
+    <Image fluid src={clubs.Image}/>
+    <Card.Content>
+      <Card.Header>{clubs.ClubName}</Card.Header>
+      <Card.Meta>
+        <span className='date'>Joined in 2015</span>
+      </Card.Meta>
+      <Card.Description>
+        {clubs.Description}
+      </Card.Description>
+    </Card.Content>
+    <Card.Content extra>
+      <a>
+        <Icon name='user' />
+        22 Friends
+      </a>
+    </Card.Content>
+  </Card>
+<Card>
+    <Image fluid src={clubs.Image}/>
+    <Card.Content>
+      <Card.Header>{clubs.ClubName}</Card.Header>
+      <Card.Meta>
+        <span className='date'>Joined in 2015</span>
+      </Card.Meta>
+      <Card.Description>
+        {clubs.Description}
+      </Card.Description>
+    </Card.Content>
+    <Card.Content extra>
+      <a>
+        <Icon name='user' />
+        22 Friends
+      </a>
+    </Card.Content>
+  </Card>
+<Card>
+    <Image fluid src={clubs.Image}/>
+    <Card.Content>
+      <Card.Header>{clubs.ClubName}</Card.Header>
+      <Card.Meta>
+        <span className='date'>Joined in 2015</span>
+      </Card.Meta>
+      <Card.Description>
+        {clubs.Description}
+      </Card.Description>
+    </Card.Content>
+    <Card.Content extra>
+      <a>
+        <Icon name='user' />
+        22 Friends
+      </a>
+    </Card.Content>
+</Card>
+<Card>
+    <Image fluid src={clubs.Image}/>
+    <Card.Content>
+      <Card.Header>{clubs.ClubName}</Card.Header>
+      <Card.Meta>
+        <span className='date'>Joined in 2015</span>
+      </Card.Meta>
+      <Card.Description>
+        {clubs.Description}
+      </Card.Description>
+    </Card.Content>
+    <Card.Content extra>
+      <a>
+        <Icon name='user' />
+        22 Friends
+      </a>
+    </Card.Content>
+</Card>
+    </Card.Group>
 	</Grid.Row>
         </Grid>
     );
